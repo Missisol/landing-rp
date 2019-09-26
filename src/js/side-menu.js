aeef79e9fa367c4a08354659a4a2a0dd8c757acc
Missisol <missisololol@gmail.com>
2019-09-26 14:10:24 +0300

    Phone header & main-screen created

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -2,16 +2,19 @@
 
 const iconWrap = document.querySelector('#iconWrap');
 const menuWrap = document.querySelector('#sideMenuWrap');
+const overlay = document.querySelector('#overlay');
 
 iconWrap.addEventListener('click', () => {
 
     if (menuWrap.classList.contains('invisible')) {
         menuWrap.classList.remove('invisible');
         menuWrap.classList.add('visible');
+        overlay.classList.remove('invisible');
         menuWrap.style.right = '0';
     } else {
         menuWrap.classList.remove('visible');
         menuWrap.classList.add('invisible');
+        overlay.classList.add('invisible');
         menuWrap.style.right = '-370px';
     }
 });
