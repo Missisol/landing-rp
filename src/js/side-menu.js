6e0333254dfb6aaaa33e2c602316503766c2d52e
Missisol <missisololol@gmail.com>
2019-09-28 01:12:45 +0300

    test commit-12

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -21,7 +21,7 @@ iconWrap.addEventListener('click', () => {
 
         overlay.classList.add('invisible');
 
-        menuWrap.style.right = '-60vw';
+        menuWrap.style.right = '-100vw';
 
         document.body.style.overflow = '';
     }
