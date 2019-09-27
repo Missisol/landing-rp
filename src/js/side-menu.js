92671f92e8bf66c2821bca335734173db7a2124b
Missisol <missisololol@gmail.com>
2019-09-28 02:46:32 +0300

    test commit-14

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -4,25 +4,30 @@ const iconWrap = document.querySelector('#iconWrap');
 const menuWrap = document.querySelector('#sideMenuWrap');
 const overlay = document.querySelector('#overlay');
 
-iconWrap.addEventListener('click', () => {
+iconWrap.addEventListener('click', (e) => {
+    e.preventDefault();
 
     if (menuWrap.classList.contains('invisible')) {
         menuWrap.classList.remove('invisible');
         menuWrap.classList.add('visible');
 
+        iconWrap.classList.add('active');
+
         overlay.classList.remove('invisible');
 
         menuWrap.style.right = '0';
 
-        document.body.style.overflow = 'hidden';
+        // document.body.style.overflow = 'hidden';
     } else {
         menuWrap.classList.remove('visible');
         menuWrap.classList.add('invisible');
+
+        iconWrap.classList.remove('active');
 
         overlay.classList.add('invisible');
 
         menuWrap.style.right = '-100vw';
 
-        document.body.style.overflow = '';
+        // document.body.style.overflow = '';
     }
 });
